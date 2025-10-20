a44600e4826f588b4a22f8b4e20ebf1a1eebc9fa
martaorpi2 <morpi@ismp.edu.ar>
2025-10-20 11:00:02 -0300

    ss

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -30,7 +30,7 @@ return [
     // The string below will be passed through the url() helper.
     // - default: '' (project root)
     // - alternative: 'admin' (the admin's dashboard)
-    'home_link' => '',
+    'home_link' => 'admin',
 
     // Content of the HTML meta robots tag to prevent indexing and link following
     'meta_robots_content' => 'noindex, nofollow',
@@ -71,7 +71,7 @@ return [
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
     //'project_logo'   => '<b>ePORRES</b>admin',
-    'project_logo'   => '<b>ePorres</b>admin',
+    'project_logo'   => '<b>Porr</b>Manager',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
